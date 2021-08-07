dd54ccf69a3e91d382b0490a593ebaee9c28dcc0
kimani <kimanihube@gmail.com>
2021-08-07 20:36:26 +0300

    New_Content_Update

---
+++ _nuxt/d75ed67.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{536:function(e,n,t){"use strict";t.r(n);var l={},r=t(26),component=Object(r.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("Here")])}),[],!1,null,null,null);n.default=component.exports}}]);
---
+++ _nuxt/static/1628357782/mpesa-integration/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/mpesa-integration", {data:[{}],fetch:{},mutations:void 0});
