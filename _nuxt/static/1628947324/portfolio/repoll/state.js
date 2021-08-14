1f4eda608157d943ebb3ca2200c3d42e55ae26f8
kimani <kimanihube@gmail.com>
2021-08-14 16:22:07 +0300

    New_Content_Update

---
+++ _nuxt/static/1628947324/portfolio/repoll/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628947324",layout:"default",error:a,state:{nav:{navDrawer:false}},serverRendered:true,routePath:"\u002Fportfolio\u002Frepoll",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
